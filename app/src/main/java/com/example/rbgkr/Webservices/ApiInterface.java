6788594ea21be09cb03be4bfd32c9732f47c5aa2
Amit Jha <amitjha4321@gmail.com>
2020-12-10 20:56:02 +0545

    Photos Fragments configured ButterKnife, ApiInterface configured, default app_bar_main customized...

--- app/src/main/java/com/example/rbgkr/Webservices/ApiInterface.java
+++ app/src/main/java/com/example/rbgkr/Webservices/ApiInterface.java
@@ -1,4 +1,14 @@
 package com.example.rbgkr.Webservices;
 
+import com.example.rbgkr.Models.Photo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+
+    @GET("photos")
+    Call<List<Photo>> getPhotos();
 }
